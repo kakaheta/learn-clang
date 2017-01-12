3409c66994faa996c06ac52122252ab7abb312e4
kakaheta <zym150806@gmail.com>
2017-01-12 15:43:02 +0800

    反向loop

--- exec15/main.c
+++ exec15/main.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
 	int i = 0;
 
 	// first way indexing
-	for (i = 0; i < count; i++) {
+	for (i = count-1; i >= 0; i--) {
 		printf("%s is %d yes alive.\n", names[i], ages[i]);
 	}
 
@@ -25,21 +25,21 @@ int main(int argc, char *argv[])
 	char **cur_name = names;
 
 	// second way using pointers
-	for (i = 0; i < count; i++) {
+	for (i = count-1; i >= 0; i--) {
 		printf("%s is %d yes alive.\n", *(cur_name + i), *(cur_age + i));
 	}
 
 	printf("----\n");
 
 	// third way are just arrays
-	for (i = 0; i < count; i++) {
+	for (i = count-1; i >= 0; i--) {
 		printf("%s is %d yes old again.\n", cur_name[i], cur_age[i]);
 	}
 
 	printf("----\n");
 
 	// fourth way with pointers in a stupid complex way
-	for (cur_name = names, cur_age = ages; (cur_age - ages) < count; cur_name++, cur_age++) {
+	for (cur_name = names+count-1, cur_age = ages+count-1; (cur_age - ages) >= count; cur_name--, cur_age--) {
 		printf("%s is %d yes old so far.\n", *cur_name, *cur_age);
 	}
 
